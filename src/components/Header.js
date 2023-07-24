e9ad086cd1f169937d8769b6297eecd80eea6d24
GitLabSJ <aeeou12@gmail.com>
2023-07-24 21:39:37 +0900

    Feat: 라우팅에 따라 Header 변화

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,35 +1,76 @@
 class Header {
   constructor($body) {
     this.$body = $body;
+    this.h1 = document.createElement("h1");
+    this.shoesLink = document.createElement("div");
+    this.clothLink = document.createElement("div");
   }
+
+  updateProduct(path) {
+    if (path === "/") {
+      this.h1.textContent = "신발 판매";
+      this.shoesLink.classList.add("active");
+      this.clothLink.classList.remove("active");
+    } else if (path === "/clothes/") {
+      this.h1.textContent = "옷 판매";
+      this.clothLink.classList.add("active");
+      this.shoesLink.classList.remove("active");
+    }
+  }
+
   render() {
     // header 요소 생성
     const header = document.createElement("header");
     header.id = "page_header";
 
+    // 현재 페이지 주소 감지
+    const currentPath = window.location.pathname;
+
+    console.log(currentPath);
+
     // h1 요소 생성
-    const h1 = document.createElement("h1");
-    h1.classList.add("header-title");
-    h1.textContent = "신발 판매";
-    header.appendChild(h1);
+    this.h1.classList.add("header-title");
+    this.updateProduct(currentPath);
+    header.appendChild(this.h1);
 
     // nav 요소 생성
     const nav = document.createElement("nav");
 
-    // a 요소 생성
-    const shoesLink = document.createElement("a");
-    shoesLink.href = "example.html";
-    shoesLink.classList.add("nav-link");
-    shoesLink.dataset.link = "";
-    shoesLink.textContent = "신발";
-    nav.appendChild(shoesLink);
+    // shoesLink 속성 설정
+    this.shoesLink.classList.add("nav-link");
+    this.shoesLink.dataset.link = "";
+    this.shoesLink.textContent = "신발";
+    this.shoesLink.style.cursor = "pointer";
 
-    const clothLink = document.createElement("a");
-    clothLink.href = "example-routing.html";
-    clothLink.classList.add("nav-link");
-    clothLink.dataset.link = "";
-    clothLink.textContent = "옷";
-    nav.appendChild(clothLink);
+    // shoesLink 클릭 이벤트
+    this.shoesLink.addEventListener("click", (e) => {
+      window.history.pushState("", "", "/");
+      this.updateProduct("/");
+      const urlChange = new CustomEvent("urlchange", {
+        detail: { href: "/" },
+      });
+      document.dispatchEvent(urlChange);
+    });
+
+    nav.appendChild(this.shoesLink);
+
+    // clothLink 속성 생성
+    this.clothLink.classList.add("nav-link");
+    this.clothLink.dataset.link = "";
+    this.clothLink.textContent = "옷";
+    this.clothLink.style.cursor = "pointer";
+
+    // clothLink 클릭 이벤트
+    this.clothLink.addEventListener("click", (e) => {
+      window.history.pushState("", "", "/clothes/");
+      this.updateProduct("/clothes/");
+      const urlChange = new CustomEvent("urlchange", {
+        detail: { href: "/clothes/" },
+      });
+      document.dispatchEvent(urlChange);
+    });
+
+    nav.appendChild(this.clothLink);
 
     // nav 요소를 header 요소에 추가
     header.appendChild(nav);
